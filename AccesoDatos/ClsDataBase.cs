2224419fe1940659783b5cb89db2dcdf31f3071c
Estebxn <decastillo807@gmail.com>
2023-06-17 19:31:01 -0500

    Agregar archivos de proyecto.

---
+++ AccesoDatos/ClsDataBase.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class ClsDataBase
+    {
+
+        #region Variables Privadas
+
+        private SqlConnection _objSqlConnection;
+        private SqlDataAdapter _objSqlDataAdapter;
+        private SqlCommand _objSqlCommand;
+        private DataSet _dsResultados;
+        private DataTable _dtParametros;
+        private string _nombreTabla, _nombreSP, _mensajeErrorDB, _valorScalar, _nombreDB;
+        private bool _scalar;
+
+        #endregion
+
+        #region Variables Públicas
+
+        public SqlConnection ObjSqlConnection { get => _objSqlConnection; set => _objSqlConnection = value; }
+        public SqlDataAdapter ObjSqlDataAdapter { get => _objSqlDataAdapter; set => _objSqlDataAdapter = value; }
+        public SqlCommand ObjSqlCommand { get => _objSqlCommand; set => _objSqlCommand = value; }
+        public DataSet DsResultados { get => _dsResultados; set => _dsResultados = value; }
+        public DataTable DtParametros { get => _dtParametros; set => _dtParametros = value; }
+        public string NombreTabla { get => _nombreTabla; set => _nombreTabla = value; }
+        public string NombreSP { get => _nombreSP; set => _nombreSP = value; }
+        public string MensajeErrorDB { get => _mensajeErrorDB; set => _mensajeErrorDB = value; }
+        public string ValorScalar { get => _valorScalar; set => _valorScalar = value; }
+        public string NombreDB { get => _nombreDB; set => _nombreDB = value; }
+        public bool Scalar { get => _scalar; set => _scalar = value; }
+
+        #endregion
+
+        #region Constructores
+
+        public ClsDataBase()
+        {
+            DtParametros = new DataTable("SpParametros");
+            DtParametros.Columns.Add("Nombre");
+            DtParametros.Columns.Add("TipoDato");
+            DtParametros.Columns.Add("Valor");
+
+            NombreDB = string.Empty;
+
+
+        }
+
+        #endregion
+
+        #region Métodos Privados
+
+        private void CrearConexionBaseDatos(ref ClsDataBase objDataBase)
+        {
+
+        }
+
+        private void ValidarConexionBaseDatos(ref ClsDataBase objDataBase)
+        {
+
+        }
+
+        private void CrearConexionBaseDatos(ref ClsDataBase objDataBase)
+        {
+
+        }
+
+        private void CrearConexionBaseDatos(ref ClsDataBase objDataBase)
+        {
+
+        }
+
+        private void CrearConexionBaseDatos(ref ClsDataBase objDataBase)
+        {
+
+        }
+
+        private void CrearConexionBaseDatos(ref ClsDataBase objDataBase)
+        {
+
+        }
+
+        #endregion
+
+        #region Métodos Públicos
+
+        #endregion
+
+    }
+}
---
+++ ProyectoPrueba/Program.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoPrueba.Principal;
+using System;
+using System.Windows.Forms;
+
+namespace ProyectoPrueba
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+        }
+    }
+}
